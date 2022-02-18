492dacb9bfe100b787350b5467272bca41c255d8
Khojiakbar <kh.tukhtasinov@gmail.com>
2022-02-18 20:12:02 +0100

    Second modification

--- js/script.js
+++ js/script.js
@@ -8,12 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Your last movie?', ''),
-      b = prompt('How will you estimate it?', ''),
-      c = prompt('Your last movie?', ''),
-      d = prompt('How will you estimate it?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Your last movie?', ''),
+          b = prompt('How will you estimate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Enough movies have watched');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are classic movielover');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are kinoman');
+} else {
+    console.log('It is error');
+}
 
 console.log(personalMovieDB);
+
