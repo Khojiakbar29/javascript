c4d368ca16a5ca278c4b551b7c658f6fc6d1bd7d
Khojiakbar <kh.tukhtasinov@gmail.com>
2022-02-18 17:40:18 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use sctrict';
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = 'alex';
+
+const bool = true;
+
+console.log('something');
+
+let und;
+console.log(und);
